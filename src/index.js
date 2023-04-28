0f3dbd72736fede598cf8257dee5ac079cea60de
hier09lyph <v.varona89@gmail.com>
2023-04-28 17:02:13 +0200

    feat: add keys into keyboard

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ header.innerHTML = "Virtual Keyboard";
 document.body.append(header);
 
 const textarea = document.createElement("textarea");
-textarea.classList = "textarea"
+textarea.classList = "textarea";
 textarea.cols = 90;
 textarea.rows = 10;
 textarea.placeholder = "The virtual keyboard was created in Windows";
@@ -18,3 +18,90 @@ const description = document.createElement("p");
 description.className = "description";
 description.innerHTML = "To switch the language combination: left ctrl + alt";
 document.body.append(description);
+
+let keyboardLayout;
+let keyboardButtons = [];
+let keyboardLang = localStorage.getItem("keyboardLanguage")
+  ? localStorage.getItem("keyboardLanguage")
+  : "ru";
+let unshifted = true;
+
+async function generateKeyboardData() {
+  try {
+    const response = await fetch("assets/keyboard.json");
+    const data = await response.json();
+    keyboardLayout = data;
+    console.log(keyboardLayout[keyboardLang]);
+    generateKeyboard(keyboardLayout[keyboardLang], unshifted);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+generateKeyboardData();
+
+class KeyboardButton {
+  constructor(keyData) {
+    this.eventCode = keyData.eventCode;
+    this.type = keyData.type;
+    this.character = keyData.character;
+    this.alternativeCharacter = keyData.alternativeCharacter;
+    this.element = this.createButtonElement(true);
+  }
+
+  createButtonElement() {
+    const button = document.createElement("div");
+    if (this.type == "functional" || this.type == "spacing") {
+      button.className = `${this.eventCode} key`;
+    } else {
+      button.className = "key";
+    }
+
+    button.textContent = this.character;
+    if (this.character == "Delete") {
+      button.textContent = "Del";
+    }
+
+    button.addEventListener("click", () => {
+      if (this.eventCode === "Tab") {
+        this.onButtonClick("    ");
+      } else if (this.eventCode === "Space") {
+        this.onButtonClick(" ");
+      } else if (this.eventCode === "Enter") {
+        this.onButtonClick("Enter");
+      } else {
+        this.onButtonClick(button.textContent);
+      }
+    });
+
+    button.addEventListener("mousedown", () => {
+      button.classList.add("pressed");
+    });
+
+    button.addEventListener("mouseup", () => {
+      button.classList.remove("pressed");
+    });
+
+    return button;
+  }
+
+  onButtonClick(text) {
+    if (text == "Enter") {
+      textarea.value += `${"\n"}`;
+    } else if (this.type !== "functional") {
+      textarea.value += text;
+    }
+  }
+}
+
+function generateKeyboard(layout, unshifted) {
+  layout.forEach((keyData) => {
+    const key = new KeyboardButton(keyData);
+    if (unshifted) {
+      keyboard.appendChild(key.element);
+    } else {
+      keyboard.appendChild(key.elementShifted);
+    }
+    keyboardButtons.push(key);
+  });
+}
